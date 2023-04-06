821b9d53d8e2924e13546a6680d55727f1bbe4fe
Shaharyar417 <shaharyar.bcss19@iba-suk.edu.pk>
2023-04-06 09:21:29 +0500

    finalised the application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { Home } from "./pages/Home";
 import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
+import {Routes, Route} from 'react-router-dom';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Account from "./pages/Account";
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
-      <Home />
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/account' element={<Account />}/>
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
